f1d95b41f09ee935019da85623de6e1823156b5d
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-15 03:59:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,7 +21,8 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gabarone"]
+  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  
 
   
   
